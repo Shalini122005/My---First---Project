691c5c2f523ee2efd3befa6a11c299b29610c10a
Shalini122005 <chsonyshalini@gmail.com>
2025-12-23 21:17:18 +0530

    two Sum.py

---
+++ two Sum.py
@@ -0,0 +1,9 @@
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}  # Maps number to its index
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i
+
+        return []  
