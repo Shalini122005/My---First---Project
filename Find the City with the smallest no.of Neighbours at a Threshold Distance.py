2b1167e8945acdf628097abd5d8c5c77f9b513a6
Shalini122005 <chsonyshalini@gmail.com>
2026-04-05 21:53:17 +0530

    Create Find the City with the smallest no.of Neighbours at a Threshold Distance.py

---
+++ Find the City with the smallest no.of Neighbours at a Threshold Distance.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
+        dist = [[float('inf')] * n for _ in range(n)]
+        for i in range(n):
+            dist[i][i] = 0
+        for u, v, w in edges:
+            dist[u][v] = w
+            dist[v][u] = w   
+        for via in range(n):
+            for i in range(n):
+                for j in range(n):
+                    if dist[i][via] + dist[via][j] < dist[i][j]:
+                        dist[i][j] = dist[i][via] + dist[via][j]
+        min_count = float('inf')
+        city = -1
+        for i in range(n):
+            count = 0
+            for j in range(n):
+                if dist[i][j] <= distanceThreshold:
+                    count += 1
+            if count <= min_count:
+                min_count = count
+                city = i        
+        return city
