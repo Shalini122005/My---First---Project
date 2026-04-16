1d71cc53fc456edf4b1a8e84cb4f64ab133dc144
Shalini122005 <chsonyshalini@gmail.com>
2026-04-16 19:35:22 +0530

    Create Longest Increasing Subsequence.py

---
+++ Longest Increasing Subsequence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        lis = []
+        for num in nums:
+            pos = bisect.bisect_left(lis, num)
+            if pos == len(lis):
+                lis.append(num)
+            else:
+                lis[pos] = num
+        return len(lis)
