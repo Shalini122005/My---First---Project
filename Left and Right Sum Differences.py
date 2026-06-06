5d50950fd9c9132d7ac130cbd1bb6d56157bc18f
Shalini122005 <chsonyshalini@gmail.com>
2026-06-06 12:41:34 +0530

    Create Left and Right Sum Differences.py

---
+++ Left and Right Sum Differences.py
@@ -0,0 +1,12 @@
+class Solution:
+    def leftRightDifference(self, nums: List[int]) -> List[int]:
+        total = sum(nums)
+        left_sum = 0
+        answer = []
+
+        for num in nums:
+            total -= num  # right sum
+            answer.append(abs(left_sum - total))
+            left_sum += num
+
+        return answer
