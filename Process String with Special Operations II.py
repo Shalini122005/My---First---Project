852d3be1bfbca5e81533b76821aea2098bb0e6f9
Shalini122005 <chsonyshalini@gmail.com>
2026-06-17 19:09:38 +0530

    Create Process String with Special Operations II.py

---
+++ Process String with Special Operations II.py
@@ -0,0 +1,43 @@
+class Solution:
+    def processStr(self, s: str, k: int) -> str:
+        n = len(s)
+
+        lengths = [0] * (n + 1)
+
+        
+        for i, ch in enumerate(s):
+            cur = lengths[i]
+
+            if 'a' <= ch <= 'z':
+                lengths[i + 1] = cur + 1
+            elif ch == '*':
+                lengths[i + 1] = max(0, cur - 1)
+            elif ch == '#':
+                lengths[i + 1] = cur * 2
+            else:  # '%'
+                lengths[i + 1] = cur
+
+        if k < 0 or k >= lengths[n]:
+            return '.'
+
+        
+        for i in range(n - 1, -1, -1):
+            ch = s[i]
+            prev_len = lengths[i]
+            cur_len = lengths[i + 1]
+
+            if 'a' <= ch <= 'z':
+                if k == cur_len - 1:
+                    return ch
+
+            elif ch == '*':
+                pass
+
+            elif ch == '#':
+                if prev_len > 0 and k >= prev_len:
+                    k -= prev_len
+
+            else:  # '%'
+                k = prev_len - 1 - k
+
+        return '.'
